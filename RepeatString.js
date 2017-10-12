074154faefc05300222019ff26f5f2a664947d3e
habeshwoub <habeshwoub@gmail.com>
2017-10-12 21:17:15 +0300

    Repeat String

---
+++ RepeatString.js
@@ -0,0 +1,13 @@
+/**Repeat a string
+Repeat a given string (first argument) num times (second argument). 
+Return an empty string if num is not a positive number. 
+use str.repeat(num) function to repeat a string num times*/
+function repeatStringNumTimes(str,num){
+	if(num>0){
+	return str.repeat(num);
+		}
+	else {
+	return "";
+	 }
+	}
+}
