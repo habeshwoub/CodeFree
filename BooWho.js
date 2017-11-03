3fad052a1afd17fd8c0c735c9eccd2f76abbcdc9
habeshwoub <habeshwoub@gmail.com>
2017-11-03 07:04:28 +0200

    Boo Who ex

---
+++ BooWho.js
@@ -0,0 +1,10 @@
+/*Boo Whoo*/
+function booWho(bool) {
+  // Check the data type of the argument bool using typeof, if boolean return true else return false
+if(typeof(bool)==="boolean"){
+  return true;
+}
+else 
+  return false;
+}
+booWho(null);
